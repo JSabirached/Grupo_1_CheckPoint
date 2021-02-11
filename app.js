eaf416d9d0dc44b14dede99fb17c9fe8b60efdaf
matias cardillo <matcardillo91@gmail.com>
2021-02-11 20:38:29 -0300

    a

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/indexRouter');
 var formRouter = require('./routes/formRouter');
 var productosRouter = require('./routes/productosRouter');
-var novedades = require('./routes/novedadesRouter');
+
 var app = express();
 
 // view engine setup
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/formularios', formRouter);
 app.use('/productos', productosRouter);
-app.use('/novedades', novedades);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
