346024f4962c761501bf1dbda59af9fbc0e5b4ac
matias cardillo <matcardillo91@gmail.com>
2021-02-22 22:03:09 -0300

    agregarproductofaltaeditaryeliminar

--- routes/productosRouter.js
+++ routes/productosRouter.js
@@ -6,7 +6,7 @@ var multer=require('multer');
 var path=require('path');
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'public/images2')
+      cb(null, 'public/images')
     },
     filename: function (req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now()+path.extname(file.originalname))
