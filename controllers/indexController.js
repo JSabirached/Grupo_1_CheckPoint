278f497e3e6016613b3a10041694c2564bf789b4
JSabirached <josueabirached@gmail.com>
2021-02-26 17:53:46 -0300

    ponele que fusion

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,16 @@
+var {getproductos,setproductos}=require("../data/platos.js");
+const fs = require('fs');
+let plato=getproductos();
+var menu=require("../data/platos.js");
+
 module.exports={
-    
+    editarfront :  (req,res)=>{
+        res.render('editar',{menu
+    })
+},
+    editarback:  (req,res)=>{
+        res.render('editar')
+    },
     index :  (req,res)=>{
         res.render('index')
     },
@@ -29,7 +40,45 @@ registro:(req,res)=>{
 
     })
 },
+/*agregar plato */
+agregarproducto:(req,res)=>{
+    res.render("agregarproducto",{plato
 
+    })
+},
+
+crearPlato:(req,res)=>{
+    var {nombrePlato,tituloPlato, description,precioPlato,descripcionPlato,cantidad}=req.body
+          
+    
+    var platos={
+              
+             id:"",
+             name: tituloPlato,
+             price: precioPlato,
+             image:req.files[0].filename,
+             category:"bebidas",
+             description:descripcionPlato,
+             
+            
+             
+          }
+         if(cantidad==1){
+   plato.bebidas.push(platos)
+  }else
+  if(cantidad==2){
+      plato.hamburguesas.push(platos)
+     }else
+     if(cantidad==3){
+      plato.postres.push(platos)
+     }else
+     if(cantidad==4){
+      plato.pizzas.push(platos)
+     }
+      setproductos(plato)
+  
+  res.redirect("agregarproducto")
+  },
 /*index*/
 cartadelivery:(req,res)=>{
     res.render("carta",{
--- data/platos.js
+++ data/platos.js
@@ -8,7 +8,11 @@ module.exports = {
     getproductos : () => {
         return JSON.parse(fs.readFileSync(platos_db, 'utf-8'));
     },
-    setproductos : (data) => {
-        fs.writeFileSync (platos_db, JSON.stringify(data, null,2), 'utf-8');
-    }
-}
+    platosFilter:function(category){
+        let platosFiltrados = platos.filter(platos=>platos.category==category)
+        return platosFiltrados
+       },
+       setproductos : (data) => {
+        fs.writeFileSync(__dirname+'/platos.json',JSON.stringify(data,null,2),'utf-8');
+    },
+ }
