ba8a24ad702f2eccf2ded40809ac48507c43e18f
matias cardillo <matcardillo91@gmail.com>
2021-03-10 21:20:02 -0300

    editar con middlewares

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -2,11 +2,17 @@ const express = require('express');
 const router = express.Router();
 const {index, comidaList, comidaCreate, comidaEdit, comidaStore, comidaDelete, comidaUpdate} = require('../controllers/adminController');
 const addValidator=require("../validations/addValidator")
-router.get('/',index);
+router.get('/',function logged(req,res,next){if(req.session.user){
+    next()
+}res.redirect("/")},index);
 
-router.get('/admin/list',comidaList);
+router.get('/admin/list',function logged(req,res,next){if(req.session.user){
+    next()
+}res.redirect("/")},comidaList);
 
-router.get('/admin/create',comidaCreate);
+router.get('/admin/create',function logged(req,res,next){if(req.session.user){
+    next()
+}res.redirect("/")},comidaCreate);
 router.post('/admin/create',addValidator,comidaStore);
 
 router.get('/admin/edit/:id',comidaEdit);
