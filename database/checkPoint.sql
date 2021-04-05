fb0b38b61cf732c23e72a3efc1a22949c4da99b8
JSabirached <josueabirached@gmail.com>
2021-04-05 12:09:13 -0300

    estructure sql

--- database/checkPoint.sql
+++ database/checkPoint.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `buy`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `buy` (
-  `idBuy` int unsigned NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `fecha_Compra` date NOT NULL,
   `fecha_Entrega` date NOT NULL,
   `horario_Entrega` datetime NOT NULL,
@@ -32,7 +32,7 @@ CREATE TABLE `buy` (
   `Factura_idFood` varchar(45) DEFAULT NULL,
   `updated_at TIMESTAMP` datetime DEFAULT NULL,
   `created_at TIMESTAMP` datetime DEFAULT NULL,
-  PRIMARY KEY (`idBuy`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -46,6 +46,30 @@ LOCK TABLES `buy` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'bebidas'),(2,'postres'),(3,'hamburguesas'),(4,'pizzaEmpa');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `food`
 --
 
@@ -53,16 +77,18 @@ DROP TABLE IF EXISTS `food`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `food` (
-  `idFood` int unsigned NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(15) NOT NULL,
   `price` varchar(6) NOT NULL,
   `image` blob,
-  `category` varchar(20) NOT NULL,
   `description` varchar(180) NOT NULL,
   `created_at TIMESTAMP` datetime DEFAULT NULL,
   `updated_at TIMESTAMP` datetime DEFAULT NULL,
-  PRIMARY KEY (`idFood`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id_category` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_food_category_idx` (`id_category`),
+  CONSTRAINT `fk_food_category` FOREIGN KEY (`id_category`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,6 +97,7 @@ CREATE TABLE `food` (
 
 LOCK TABLES `food` WRITE;
 /*!40000 ALTER TABLE `food` DISABLE KEYS */;
+INSERT INTO `food` VALUES (1,'Fernet','450',NULL,'Jarron lts',NULL,NULL,1),(2,'Vino','250',NULL,'cosecha Mendoza1865',NULL,NULL,1),(3,'Brownie','80',NULL,'Chocolate Suizo',NULL,NULL,2),(4,'Helado2bocha','50',NULL,'Dos bochas con su recorrido de chocolate',NULL,NULL,2),(5,'DobleQueso','250',NULL,'Doble con Queso',NULL,NULL,3),(6,'Completa','230',NULL,'Jamon Queso Huevo ',NULL,NULL,3),(7,'Pizza Anchoa','300',NULL,'Pizza con anchoa de la mejor seleccion',NULL,NULL,4),(8,'Muzzarella','290',NULL,'Muzza con extraa Queso!!',NULL,NULL,4),(9,'KamgreBurguer','200',NULL,'traida con la formula Secreta',NULL,NULL,3);
 /*!40000 ALTER TABLE `food` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,20 +109,21 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `idUser` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(10) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `pass` varchar(12) NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `pass` varchar(70) NOT NULL,
   `avatar` blob,
-  `category` varchar(20) NOT NULL,
+  `category` int NOT NULL,
   `provincia` varchar(45) DEFAULT NULL,
   `localidad` varchar(45) DEFAULT NULL,
   `direccion` varchar(45) DEFAULT NULL,
-  `telefono` varchar(10) DEFAULT NULL,
+  `telefono` varchar(45) DEFAULT NULL,
   `updated_at TIMESTAMP` datetime DEFAULT NULL,
   `created_at TIMESTAMP` datetime DEFAULT NULL,
-  PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `surname` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -104,12 +132,9 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Josue','josueabirached@gmail.com','123123',NULL,1,'Buenos Aires','Lanus','Av.mi casa','1123920244',NULL,NULL,'Abi Rached');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'checkpoint'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -120,4 +145,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-24 10:59:03
+-- Dump completed on 2021-04-02 12:44:58
