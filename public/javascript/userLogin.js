11bf9a50930790a6c4a72e6b7ec7106a8f419439
anytaPato <anytapatorojas@gmail.com>
2021-04-06 00:11:57 -0300

    validación front login en proceso

---
+++ public/javascript/userLogin.js
@@ -0,0 +1,56 @@
+let qs = function(element) {
+    return document.querySelector(element)
+
+}
+
+window.addEventListener('load', function() {
+
+
+    let $inputEmail = qs('#email'),
+    $emailErrors = qs('#emailErrors'),
+
+    $Pass = qs('#pass'),
+    $passErrors = qs('#passErrors'),
+
+    regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
+    regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
+
+ $inputEmail.addEventListener('blur', function(){
+    switch (true) {
+        case !$inputEmail.value.trim():
+            $emailErrors.innerHTML = 'El campo email es obligatorio';
+            $inputEmail.classList.add('invalido')
+            break;
+        case !regExEmail.test($inputEmail.value):
+            $emailErrors.innerHTML = 'Debes ingresar un Email válido'
+            $inputEmail.classList.add('invalido')
+            break;
+        default:
+            $inputEmail.classList.remove('invalido');
+            $inputEmail.classList.add('valido');
+            $emailErrors.innerHTML = ''
+            break;
+    }
+
+})
+
+$Pass.addEventListener('blur', function(){
+    switch (true) {
+        case !$Pass.value.trim():
+            $passErrors.innerHTML = 'El campo contraseña es obligatorio.';
+            $Pass.classList.add('invalido')
+            break;
+        case !regExPass.test($Pass.value):
+            $passErrors.innerHTML = 'Debe tener entre 6 y 12 caracteres, al menos una mayúscula, una minúscula y un número.'
+            $Pass.classList.add('invalido')
+            break;
+        default:
+            $Pass.classList.remove('invalido');
+            $Pass.classList.add('valido');
+            $passErrors.innerHTML = ''
+            break;
+    }
+
+})
+
+})
