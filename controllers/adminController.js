cf04a781bebf91f14ba7bc082feec5bb0a73e377
JSabirached <josueabirached@gmail.com>
2021-04-07 00:05:44 -0300

    cambios env

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,84 +1,79 @@
-const {validationResult}=require('express-validator');
+const { validationResult } = require('express-validator');
 const db = require('../database/models');
-const {Op} = require('sequelize');
+const { Op } = require('sequelize');
 
 module.exports = {
-    index : (req,res) => {
+    index: (req, res) => {
         res.render('admin/index')
     },
-    comidaList : (req,res) => {
+    comidaList: (req, res) => {
 
-         db.Food.findAll({include:[
-            {association: 'food'},
-        ]})
-        
+        db.Food.findAll()
 
-       
-        .then(food => {
+
+
+            .then(food => {
                 res.render("admin/comidaList", {
                     title: "Lista de Comidas",
-                  food
+                    food
 
                 })
             })
             .catch(error => console.log(error))
     },
-    userList : (req,res) => {
+    userList: (req, res) => {
 
-       db.User.findAll({include:[
-            {association: 'user'},
-        ]})
-        
-        .then(user => {
+        db.Usuario.findAll()
+
+            .then(users => {
                 res.render("admin/userList", {
                     title: "Lista de Usuarios",
-                  
-                 user
+                    users
                 })
             })
             .catch(error => console.log(error))
     },
-    ventasList : (req,res) => {
+    ventasList: (req, res) => {
 
-       db.Buy.findAll({include:[
-            {association: 'buy'},
-        ]})
-        
+        db.Buy.findAll()
 
-       
-        .then(buy => {
+
+
+            .then(buy => {
                 res.render("admin/ventas", {
                     title: "ventas actuales",
-                  
-                buy
+
+                    buy
                 })
             })
             .catch(error => console.log(error))
     },
-    categorias : (req,res) => {
+    categorias: (req, res) => {
 
         db.Category.findAll()
-       
 
-       
-        .then(categorias => {
-                res.render("admin/categorias", {  title: "Categorias",
-           categorias     })
+
+
+            .then(categorias => {
+                res.render("admin/categorias", {
+                    title: "Categorias",
+                    categorias
+                })
             })
             .catch(error => console.log(error))
     },
-   
-   
-   
-   
-    comidaCreate : (req,res) => {
-       res.render('admin/comidaCreate')
 
-      
-    
+
+
+
+    comidaCreate: (req, res) => {
+        res.render('admin/comidaCreate')
+
+
+
     },
-    comidaStore : (req,res) =>{
-        if(errors.isEmpty()){
+    comidaStore: (req, res) => {
+        if (errors.isEmpty()) {
             db.Comidas.create({
                 name: req.name,
                 price: Number(req.price),
@@ -86,28 +81,28 @@ module.exports = {
                 category: req.category,
                 description: req.description
             })
-            .then(()=>{
-                res.redirect('/admin')
-           })
-           .catch(error=> res.send(error))
+                .then(() => {
+                    res.redirect('/admin')
+                })
+                .catch(error => res.send(error))
 
         }
     }
-,
-    comidaEdit : (req,res) => {
-        
+    ,
+    comidaEdit: (req, res) => {
+
         let comida = comidas.find(comida => comida.id === +req.params.id);
 
-        res.render('admin/comidaEdit',{
-           id:req.params.id
+        res.render('admin/comidaEdit', {
+            id: req.params.id
         })
     },
-    comidaUpdate : (req,res) => {
-        
-        const {name,price,category,description,image} = req.body;
+    comidaUpdate: (req, res) => {
+
+        const { name, price, category, description, image } = req.body;
 
         comidas.forEach(comida => {
-            if(comida.id === +req.params.id){
+            if (comida.id === +req.params.id) {
                 comida.id = Number(req.params.id);
                 comida.name = name;
                 comida.price = price;
@@ -116,25 +111,25 @@ module.exports = {
                 comida.image = image
             }
         });
-        var errores=validationResult(req)
-        if(!errores.isEmpty()){
-            
-            return res.render("admin/comidaEdit",{errores:errores.mapped(),old:req.body, id:req.params.id})
-                
-                    }
-        fs.writeFileSync('./database/models',JSON.stringify(Comidas),'utf-8');
+        var errores = validationResult(req)
+        if (!errores.isEmpty()) {
+
+            return res.render("admin/comidaEdit", { errores: errores.mapped(), old: req.body, id: req.params.id })
+
+        }
+        fs.writeFileSync('./database/models', JSON.stringify(Comidas), 'utf-8');
         res.redirect('/admin');
     },
-    comidaDelete : (req,res) => {
+    comidaDelete: (req, res) => {
         comidas.forEach(comida => {
-            if(comida.id === +req.params.id){
+            if (comida.id === +req.params.id) {
                 var aEliminar = comidas.indexOf(comida);
-                comidas.splice(aEliminar,1)
+                comidas.splice(aEliminar, 1)
             }
         });
-        
-        fs.writeFileSync('./database/models',JSON.stringify(Comidas),'utf-8');
+
+        fs.writeFileSync('./database/models', JSON.stringify(Comidas), 'utf-8');
         res.redirect('/admin');
     }
 }
- 
+
