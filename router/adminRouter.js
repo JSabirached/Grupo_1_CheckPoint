1b64f0edeca1707ad8f8fbbc83d5ff9e7a2374dc
JSabirached <josueabirached@gmail.com>
2021-04-10 02:42:33 -0300

    Edit Post funcionando sin validaciones

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {index, comidaList, comidaCreate,comidaStore, ventasList,userList,categorias,comidaEdit,detalleProducto,  actualizarProducto, borrarProducto,comidas} = require("../controllers/adminController");
+const {index, comidaList, comidaCreate,comidaStore, ventasList,userList,categorias,comidaEdit,comidaUpdate,detalleProducto,  actualizarProducto, borrarProducto,comidas} = require("../controllers/adminController");
 
 const uploadImages = require("../middlewares/uploadImages");
 const addValidator = require("../validations/addValidator");
@@ -17,7 +17,7 @@ router.get('/create',comidaCreate);
 router.post('/create',uploadImages.any(),addValidator,comidaStore);
 
 router.get('/edit/:id',comidaEdit);
-//router.put('/update/:id',addValidator,comidaUpdate);
+router.put('/update/:id',uploadImages.any(),addValidator,comidaUpdate);
 
 //router.delete('/delete/:id',comidaDelete);
 
